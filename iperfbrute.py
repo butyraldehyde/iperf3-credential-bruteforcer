fe4c7203c534957f8d9d03ac1ff4edbf015b0adc
butyraldehyde <99754153+butyraldehyde@users.noreply.github.com>
2025-09-17 21:03:00 -0700

    Add files via upload

---
+++ iperfbrute.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import argparse
+from hashlib import sha256
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--username','-u',type=str,help='Username string.')
+parser.add_argument('--wordlist',required=True,help='Wordlist path.')
+parser.add_argument('--hash',help='Hash string, should just be sha256 hash.')
+parser.add_argument('--csv',help='Local CSV path.')
+args=parser.parse_args()
+
+
+def hashcheck(funcUsername,funcHash):
+    try:
+        with open(args.wordlist, 'r', encoding='ISO-8859-1') as f:
+            for password in f:
+                testString = f"{{{funcUsername.strip()}}}{password.strip()}"
+                if sha256(testString.encode()).hexdigest() == funcHash:
+                    print(f"The password for {funcUsername} is likely {password}")
+                    return
+            print(f"Couldn't find the password for {funcUsername}")
+    except FileNotFoundError:
+        print("File not found.")
+        exit(1)
+    except Exception as Err:
+        print(f"Something went wrong. The error is: {Err}")
+        exit(1)
+
+
+
+if args.csv:
+    if args.username:
+        print("--csv option cannot be used with --username")
+        exit(1)
+    if args.hash:
+        print("--csv option cannot be used with --hash")
+        exit(1)
+    try:
+        with open(args.csv,'r') as csvfile:
+            for row in csvfile:
+                if row.startswith('#'):
+                    continue
+                if ',' not in row:
+                    continue
+                print()
+                print(f"Checking wordlist for credentials for {row}")
+                elements=row.split(',')
+                username=elements[0].strip()
+                CSVhash=elements[1].strip()
+                hashcheck(username,CSVhash)
+    except FileNotFoundError:
+        print("File not found")
+        exit(1)
+    except Exception as error:
+        print(f"Something went wrong. The error is: {error}")
+        exit(1)
+else:
+    if not args.username:
+        args.username = input("Username string: ")
+        if not args.username:
+            print("No username defined. Defaulting to: 'iperf-user'.")
+            args.username = 'iperf-user'
+    if not args.hash:
+        args.hash = input("Hash string: ")
+        if not args.hash:
+            print("No hash provided. Please define one on the command line with --hash.")
+            exit(1)
+    print()
+    print(f"Checking wordlist for credentials for {args.username}")
+    hashcheck(args.username,args.hash)
+
+
+
+exit(0)
+
